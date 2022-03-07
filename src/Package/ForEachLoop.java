5da19d8139a92747169743e5df8d53273b8d2bff
sanjanenic <sanjanenicbl@gmail.com>
2022-03-07 09:12:28 +0100

    For Each prvi primjer, radi

--- src/Package/ForEachLoop.java
+++ src/Package/ForEachLoop.java
@@ -1,15 +1,19 @@
 package Package;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ForEachLoop {
     public static void main(String[] args) {
          int [] array = new int[]{1,2,3,4,5};
         System.out.println(Arrays.toString(array));
+
          for(int number : array){ //: nam govori gdje treba gledati, ovdje prvi put gleda zadnji element
 
              array[number-1]+= 12;
          }
         System.out.println(Arrays.toString(array));
+
+      //  ArrayList<Integer>list = new ArrayList<>();  ovo jos nismo radili smo je primjer kako izgleda, kod liste je nebitno mjesto na kome se elemnt nalazi
     }
 }
